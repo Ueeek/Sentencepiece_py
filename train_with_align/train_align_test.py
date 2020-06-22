d3f6dd8e348ebc9758f0b2065394512bf08fe9eb
Ueeek <ueki.k.ae@m.titech.ac.jp>
2020-06-22 14:37:51 +0900

    merge debug and alpha

--- train_with_align/train_align_test.py
+++ train_with_align/train_align_test.py
@@ -3,15 +3,21 @@ import sys
 sys.path.append("../src/")
 from UnigramTrainerAlign import train_align
 
+
+debug=False
 if __name__ == "__main__":
     arg_mini_en = {
-        "file": "../corpus/train5K.en",
+        "file": "../corpus/mini.en",
         "voc": "./res_voc/dummy.en.voc",
         "use_original_make_seed":True,
+        "debug":debug,
+        "desired_voc_size":1500,
     }
     arg_mini_ja = {
-        "file": "../corpus/train5K.jap",
+        "file": "../corpus/mini.jap",
         "voc": "./res_voc/dummy.jap.voc",
         "use_original_make_seed":True,
+        "debug":debug,
+        "desired_voc_size":1500,
     }
-    train_align(arg_mini_en,arg_mini_ja)
+    train_align(arg_mini_en,arg_mini_ja,debug=debug)
